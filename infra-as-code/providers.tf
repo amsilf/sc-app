ec1a2fe4cfd51f09d1eb67c73611e83f500902ea
Alexander Makarov <amsilf@gmail.com>
2024-07-11 15:55:01 +0800

    providers updated

--- infra-as-code/providers.tf
+++ infra-as-code/providers.tf
@@ -9,15 +9,12 @@ terraform {
   required_version = ">= 0.12.0"
 
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "=1.4.0"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=2.49.0"
     }
   }
+
   backend "azurerm" {
   }
 }
