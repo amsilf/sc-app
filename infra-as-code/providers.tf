4556b8610522804f79d3681c43ea48f147a8f13c
Alexander Makarov <amsilf@gmail.com>
2024-07-11 20:43:49 +0800

    VM to generate a plan

--- infra-as-code/providers.tf
+++ infra-as-code/providers.tf
@@ -13,6 +13,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=2.49.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = ">=3.0.0"
+    }
+
   }
 
   backend "azurerm" {
